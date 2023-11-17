573828eb354411f2bd9fa4c27f6730c65eaba0fd
cding888 <151079926+cding888@users.noreply.github.com>
2023-11-16 22:43:48 -0500

    Update fetchCurrentTemperature.ts

--- src/fetchCurrentTemperature.ts
+++ src/fetchCurrentTemperature.ts
@@ -7,6 +7,23 @@ interface TemperatureReading {
 }
 
 export function fetchCurrentTemperature(coords: GeoCoord): Promise<TemperatureReading> {
-  // TODO
-  return new Promise(res => res({ time: [], temperature_2m: [] }));
-}
+  const searchURL = new URL("https://api.open-meteo.com/v1/forecast");
+  searchURL.searchParams.set("latitude", coords.lat.toString());
+  searchURL.searchParams.set("longitude", coords.lon.toString());
+  searchURL.searchParams.set("hourly", "temperature_2m");
+  searchURL.searchParams.set("temperature_unit", "fahrenheit");
+
+  return fetch(searchURL.toString())
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(new Error(`HTTP error! Status: ${response.status}`));
+      }
+      return response.json();
+    })
+    .then(json => {
+      if (json.hourly !== undefined && json.hourly.time !== undefined && json.hourly.temperature_2m !== undefined) {
+        return Promise.resolve({ time: json.hourly.time, temperature_2m: json.hourly.temperature_2m });
+      } else {
+        return Promise.reject(new Error("No results found for query"));
+      }
+    });
