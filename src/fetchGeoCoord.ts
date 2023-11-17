42168cf634cf162980e8ef7d86af54b06951e9df
cding888 <151079926+cding888@users.noreply.github.com>
2023-11-16 22:45:05 -0500

    Update fetchGeoCoord.ts

--- src/fetchGeoCoord.ts
+++ src/fetchGeoCoord.ts
@@ -6,6 +6,27 @@ export interface GeoCoord {
 }
 
 export function fetchGeoCoord(query: string): Promise<GeoCoord> {
-  // TODO
-  return new Promise(res => res({ lat: NaN, lon: NaN }));
+  const baseUrl = "https://220.maxkuechen.com/geoCoord/search";
+  const apiUrl = `${baseUrl}?q=${encodeURIComponent(query)}`;
+
+  return fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch geo-coordinate. Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.length === 0) {
+        throw new Error("No results found for query.");
+      }
+      const firstResult = data[0];
+      const lon = Number.parseFloat(firstResult.lon);
+      const lat = Number.parseFloat(firstResult.lat);
+
+      if (isNaN(lon) || isNaN(lat)) {
+        throw new Error("Invalid geo-coordinate format in the response.");
+      }
+      return { lon, lat };
+    });
 }
