8c09b5f3804e344b6e0deef3425d22143f81127a
cding888 <151079926+cding888@users.noreply.github.com>
2023-11-19 17:19:03 -0500

    Update fetchGeoCoord.test.ts

--- src/fetchGeoCoord.test.ts
+++ src/fetchGeoCoord.test.ts
@@ -12,4 +12,33 @@ describe("fetchGeoCoord", () => {
       assert(Object.keys(result).length === 2); // Assert there are only two keys in the object
     });
   });
+  it("returns an Error when query doesn't return any results", () => {
+    const promise = fetchGeoCoord("%^#@%");
+    return promise.catch(result => {
+      assert(result instanceof Error);
+      assert(result.message === "No results found for query.");
+    });
+  });
+
+  it("correctly returns lat and lon for a query with only one result", () => {
+    const promise = fetchGeoCoord("University of Massachusetts Amherst");
+
+    return promise.then(result => {
+      assert(typeof result === "object");
+      assert(result.lon === -72.52991477067445);
+      assert(result.lat === 42.3869382);
+      assert(Object.keys(result).length === 2);
+    });
+  });
+
+  it("correctly returns the first lat and lon for a query with more than one result", () => {
+    const promise = fetchGeoCoord("connecticut chipotle");
+
+    return promise.then(result => {
+      assert(typeof result === "object");
+      assert(result.lon === -72.9269187);
+      assert(result.lat === 41.3063497);
+      assert(Object.keys(result).length === 2);
+    });
+  });
 });
